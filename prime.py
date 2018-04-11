2cea6764f132d6e92c18cf7c43b01daf0c59d008
pradeepbasha <37371601+pradeepbasha@users.noreply.github.com>
2018-04-11 18:56:18 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+a=input(enter the number())
+if(a>1) and (a<1000):
+  for i in range(2,a):
+  if(a%i==0):
+  print("it is not a prime number")
+  break
+  print("it is a prime number")
+
+  
+  
+   
