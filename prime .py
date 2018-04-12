70a295a78bf7095661250d0cc92019c4a8bf9a8a
pradeepbasha <37371601+pradeepbasha@users.noreply.github.com>
2018-04-12 18:05:23 +0530

    Create prime .py

---
+++ prime .py
@@ -0,0 +1,11 @@
+a=int(input(enter the a number))
+b=int(input(enter the b number))
+for num i in range(a,b+1):
+   num > 1:
+    for i in range(2,num):
+     ifnum(num%i==0):
+       break
+ else:
+   print("num")
+   
+  
